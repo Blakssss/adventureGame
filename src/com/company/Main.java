1538a47d2b631db33f8499337531a3f25c77d994
Mathias <mathias.blak@gmail.com>
2022-03-14 11:24:03 +0100

    solution2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner go = new Scanner(System.in);
-        String where = go.nextLine();
-        if (where.equals("east")){
-
-        }
-        if (where.equals("west")){
-
-        }
-        if (where.equals("north")){
-
-        }
-        if (where.equals("south")){
-
-        }
         Room room1 = new Room("You're in room 1","There's a bunch of skeletons here");
         Room room2 = new Room("You're in room 2","There's a bunch of bunnies here");
         Room room3 = new Room("You're in room 3","There's a bunch of gold here");
@@ -31,11 +18,68 @@ public class Main {
         room1.setSouth(room4);
         room1.setNorth(null);
         room1.setWest(null);
+        room2.setEast(room3);
+        room2.setSouth(null);
+        room2.setNorth(null);
+        room2.setWest(room1);
+        room3.setEast(null);
+        room3.setSouth(room6);
+        room3.setNorth(null);
+        room3.setWest(room2);
+        room4.setEast(null);
+        room4.setSouth(room7);
+        room4.setNorth(room1);
+        room4.setWest(null);
+        room5.setEast(null);
+        room5.setSouth(room8);
+        room5.setNorth(null);
+        room5.setWest(null);
+        room6.setEast(null);
+        room6.setSouth(room9);
+        room6.setNorth(room3);
+        room6.setWest(null);
+        room7.setEast(room8);
+        room7.setSouth(null);
+        room7.setNorth(room4);
+        room7.setWest(null);
+        room8.setEast(room9);
+        room8.setSouth(null);
+        room8.setNorth(room5);
+        room8.setWest(room7);
+        room9.setEast(null);
+        room9.setSouth(null);
+        room9.setNorth(room6);
+        room9.setWest(room8);
+
+        String where = go.nextLine();
+        if (where.equals("east")){
+
+        }
+        if (where.equals("west")){
+
+        }
+        if (where.equals("north")){
+
+        }
+        if (where.equals("south")){
+
+        }
+        if (where.equals("exit")){
+            System.out.println("Thank you for playing.");
+        }
+        if (where.equals("help")){
+            System.out.println("you stupid or something?");
+        }
+        if (where.equals("look")){
+            System.out.println("omg, we JUST told you." + currentRoom);
+        }
+
+
+
 
         System.out.println(room1.getName());
         System.out.println(room1.getDescription());
         System.out.println(currentRoom.getName());
-
 
     }
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class Room {
-    private  String name;
-    private  String description;
+    private String name;
+    private String description;
     private Room currentRoom;
     private Room east;
     private Room south;
@@ -10,16 +10,16 @@ public class Room {
     private Room west;
 
     void setWest(Room west){
-
+        this.west=west;
     }
     void setNorth(Room north){
-
+        this.north=north;
     }
     void setSouth(Room south){
-
+        this.south=south;
     }
     void setEast(Room east){
-
+        this.east=east;
     }
     void setCurrentRoom(Room currentRoom){
         this.currentRoom=currentRoom;
@@ -39,10 +39,27 @@ public class Room {
     String getDescription(){
         return description;
     }
+    Room getWest(){
+        return west;
+    }
+    Room getNorth(){
+        return north;
+    }
+    Room getEast(){
+        return east;
+    }
+    Room getSouth(){
+        return south;
+    }
+
     Room(String name, String description){
         setDescription(description);
         setName(name);
         setCurrentRoom(currentRoom);
+        setEast(east);
+        setSouth(south);
+        setNorth(north);
+        setWest(west);
     }
 }
 
