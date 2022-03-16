388ec9a65da6a566c3a232d7f139fd491946f9ec
Mathias <mathias.blak@gmail.com>
2022-03-16 21:43:50 +0100

    100% DONE

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -1,8 +1,6 @@
 package com.company;
 
 public class UserInterface {
-    Player player = new Player();
-    String where = player.getWhere();
     Map map = new Map();
     Room currentRoom = map.getStartRoom();
 
@@ -10,7 +8,6 @@ public class UserInterface {
         System.out.println("Welcome to the dungeon of doom. " + currentRoom.getName() + currentRoom.getDescription());
     }
     public void help(){
-        if (where.equals("help")) {
             System.out.println("""
                     Your commands are as follows:
                     type east to go to the room to the east.
@@ -20,17 +17,12 @@ public class UserInterface {
                     type help to all of this information again.
                     type look to ge the room description again.
                     type exit to exit this fun fun fun game.""");
-        }
     }
     public void look(){
-        if (where.equals("look")) {
             System.out.println("omg, we JUST told you." + currentRoom.getName() + currentRoom.getDescription());
-        }
     }
     public void exit(){
-        if (where.equals("exit")) {
             System.out.println("Thank you for playing.");
-        }
     }
 }
 
