1c2e1af160a3a44bd7d195ddd1b1522e19cb2719
Mathias <mathias.blak@gmail.com>
2022-03-18 14:59:27 +0100

    Starting Item stuff

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -16,8 +16,6 @@ public class Map {
         room2.setName("You're in room 2. ");
         room2.setDescription("There's a bunch of bunnies here.");
         room2.getItem().add(new Item("bunny"));
-        //Item sword = new Item();
-        //room2.addItem(sword);
 
         Room room3 = new Room();
         room3.setName("You're in room 3. ");
